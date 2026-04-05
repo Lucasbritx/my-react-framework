acad1b0af93b93029a9db759d8b8e7b26e1bc0b5
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2026-04-05 10:01:34 -0300

    feat: luact create element

---
+++ luact.js
@@ -0,0 +1,28 @@
+const TEXT_ELEMENT = "TEXT_ELEMENT"
+
+function createTextElement(text) {
+  return {
+    type: TEXT_ELEMENT,
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  }
+}
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+       children: children.map(child =>
+        typeof child === "object"
+          ? child
+          : createTextElement(child)
+      ),
+    },
+  }
+}
+export const Luact = {
+  createElement,
+}
