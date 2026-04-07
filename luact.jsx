1a9be37009ec85f8f816aa8e7eefdc78ac385323
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2026-04-06 21:52:44 -0300

    feat: refactor render function to use nextUnitOfWork for scheduling

--- luact.jsx
+++ luact.jsx
@@ -38,21 +38,14 @@ function createDom(fiber) {
   return dom
 }
 
+
 function render(element, container) {
-  const dom =
-    element.type == TEXT_ELEMENT
-      ? document.createTextNode("")
-      : document.createElement(element.type);
-
-  const isProperty = (key) => key !== "children";
-  Object.keys(element.props)
-    .filter(isProperty)
-    .forEach((name) => {
-      dom[name] = element.props[name];
-    });
-
-  element.props.children.forEach((child) => render(child, dom));
-  container.appendChild(dom);
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  }
 }
 
 let nextUnitOfWork = null
@@ -71,9 +64,51 @@ function workLoop(deadline) {
 requestIdleCallback(workLoop)
 
 function performUnitOfWork(nextUnitOfWork) {
-  // TODO
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+
+  const elements = fiber.props.children
+  let index = 0
+  let prevSibling = null
+
+  while (index < elements.length) {
+    const element = elements[index]
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    }
+  }
+
+  if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+
+    prevSibling = newFiber
+    index++
+
+    if (fiber.child) {
+    return fiber.child
+    }
+    let nextFiber = fiber
+    while (nextFiber) {
+      if (nextFiber.sibling) {
+        return nextFiber.sibling
+      }
+    nextFiber = nextFiber.parent
+  }
+
 }
-​
+
 
 export const Luact = {
   createElement,
