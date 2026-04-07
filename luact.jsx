02d12525db13a7bce8c6d7152b6c8dcd6fe58684
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2026-04-06 21:35:21 -0300

    feat: add createDom function

--- luact.jsx
+++ luact.jsx
@@ -22,6 +22,22 @@ function createElement(type, props, ...children) {
   };
 }
 
+function createDom(fiber) {
+  const dom =
+    fiber.type == TEXT_ELEMENT
+      ? document.createTextNode("")
+      : document.createElement(fiber.type)
+​
+  const isProperty = key => key !== "children"
+  Object.keys(fiber.props)
+    .filter(isProperty)
+    .forEach(name => {
+      dom[name] = fiber.props[name]
+    })
+​
+  return dom
+}
+
 function render(element, container) {
   const dom =
     element.type == TEXT_ELEMENT
