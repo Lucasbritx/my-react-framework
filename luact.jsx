adbe5f4c3ee56755b6f93561f0056740deb52f0a
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2026-04-08 23:54:51 -0300

    refactor: simplify createDom function by utilizing updateDom for property assignment

--- luact.jsx
+++ luact.jsx
@@ -28,12 +28,7 @@ function createDom(fiber) {
       ? document.createTextNode("")
       : document.createElement(fiber.type);
 
-  const isProperty = (key) => key !== "children";
-  Object.keys(fiber.props)
-    .filter(isProperty)
-    .forEach((name) => {
-      dom[name] = fiber.props[name];
-    });
+  updateDom(dom, {}, fiber.props);
 
   return dom;
 }
